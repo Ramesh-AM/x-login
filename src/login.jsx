0ba187ecb28fa54109cd202d24f800387646c6e5
ramesham3818 <ramesham3818@gmail.com>
2025-08-28 21:06:29 +0530

    Inital commit

---
+++ src/login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export default function XLogin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      setMessage("Both fields are required");
+      return;
+    }
+
+    if (username === "user" && password === "password") {
+      setMessage("Welcome, user!");
+    } else {
+      setMessage("Invalid username or password");
+    }
+  };
+
+  return (
+    <div >
+        <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username: </label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Password: </label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
